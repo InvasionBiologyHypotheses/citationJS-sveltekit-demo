9945357654186b2f6ab09ec100f39ccedb80651c
Steph Tyszka <steph@bootsa.net>
2022-03-03 15:20:44 +0100

    update front page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>A super simple example of using Citation-JS in a Svelte-Kit app.</p>
+<p>Just enter a wikidata entity id into the url.</p>
+<p>
+  For instance <a href="/Q21972834">Q21972834</a>
+</p>
